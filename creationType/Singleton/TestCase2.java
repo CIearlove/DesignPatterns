9bf73f3d947a8ed43943006ab71e645593ea027c
舵主 陈 <617316241@qq.com>
2019-04-17 14:59:38 +0800

    饿汉式-线程安全 测试用例

---
+++ creationType/Singleton/TestCase2.java
@@ -0,0 +1,35 @@
+package Singleton;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestCase2 {
+
+	public static void main(String[] args) {
+		
+		ExecutorService executor = Executors.newCachedThreadPool();
+		
+		for(int i=0;i<100;i++){
+			executor.execute(new Instantiate2());
+		}
+		
+		executor.shutdown();
+		
+		//等到所有的任务都结束
+		while(!executor.isTerminated()){
+			
+		}
+		System.out.println(Singleton2.numberOfInstantiations());
+		
+		
+	}
+}
+	
+class Instantiate2 implements Runnable{
+
+	@Override
+	public void run() {
+		Singleton2.getUniqueInstance();
+	}
+	
+}
