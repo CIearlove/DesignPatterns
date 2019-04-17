e7a7044564a2d8f7ace75c527f15437232f15701
舵主 陈 <617316241@qq.com>
2019-04-17 15:01:52 +0800

    懒汉式-线程安全 测试用例

---
+++ creationType/Singleton/TestCase3.java
@@ -0,0 +1,35 @@
+package Singleton;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestCase3 {
+
+	public static void main(String[] args) {
+		
+		ExecutorService executor = Executors.newCachedThreadPool();
+		
+		for(int i=0;i<100;i++){
+			executor.execute(new Instantiate3());
+		}
+		
+		executor.shutdown();
+		
+		//等到所有的任务都结束
+		while(!executor.isTerminated()){
+			
+		}
+		System.out.println(Singleton3.numberOfInstantiations());
+		
+		
+	}
+}
+	
+class Instantiate3 implements Runnable{
+
+	@Override
+	public void run() {
+		Singleton3.getUniqueInstance();
+	}
+	
+}
