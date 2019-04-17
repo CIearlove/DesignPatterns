acdeac530cb88e15fa2fdc304e504dfb2efffc94
舵主 陈 <617316241@qq.com>
2019-04-17 14:50:00 +0800

    增加计数器

--- creationType/Singleton/Singleton2.java
+++ creationType/Singleton/Singleton2.java
@@ -1,5 +1,7 @@
 package Singleton;
 /**
+ * 饿汉式-线程安全
+ * 
  * 线程不安全问题主要是由于 uniqueInstance 被实例化多次，采取直接实例化 uniqueInstance 的方式就不会产生线程不安全问题。
  * 
  * 但是直接实例化的方式也丢失了延迟实例化带来的节约资源的好处。
@@ -7,7 +9,9 @@ package Singleton;
  *
  */
 public class Singleton2 {
+	
 	private static Singleton2 uniqueInstance = new Singleton2();
+	private static int count;
 	
 	private Singleton2() {
     }
@@ -15,7 +19,12 @@ public class Singleton2 {
     public static Singleton2 getUniqueInstance() {
         if (uniqueInstance == null) {
             uniqueInstance = new Singleton2();
+            count++;
         }
         return uniqueInstance;
+    }
+    
+    public static int numberOfInstantiations(){
+    	return count;
     }
 }
